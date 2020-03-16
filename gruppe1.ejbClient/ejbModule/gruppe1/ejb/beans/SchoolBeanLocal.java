b689357fd1eb8815b989e0ce3bcfc81690e47423
Spilje <silkes@outlook.com>
2020-03-16 13:55:32 +0100

    Entity, DTO and beans created

---
+++ gruppe1.ejbClient/ejbModule/gruppe1/ejb/beans/SchoolBeanLocal.java
@@ -0,0 +1,18 @@
+package gruppe1.ejb.beans;
+
+import java.util.List;
+import javax.ejb.Local;
+import gruppe1.ejbClient.entity.SchoolDTO;
+
+@Local
+public interface SchoolBeanLocal {
+	void create(SchoolDTO school);
+	
+	void update(SchoolDTO school);
+	
+	void delete(int id);
+
+	SchoolDTO get(int id);
+	
+	List<SchoolDTO> getAll();
+}
