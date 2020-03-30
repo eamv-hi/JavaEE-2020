dfa92a28a57595b2c662b33eebe5632695b14f29
Spilje <silkes@outlook.com>
2020-03-30 13:47:08 +0200

    Editable table view implemented, but not working

--- gruppe1-web/src/gruppe1/web/SchoolOverview.java
+++ gruppe1-web/src/gruppe1/web/SchoolOverview.java
@@ -4,8 +4,12 @@ import java.io.Serializable;
 import java.util.List;
 
 import javax.enterprise.context.RequestScoped;
+import javax.faces.application.FacesMessage;
+import javax.faces.context.FacesContext;
 import javax.inject.Inject;
 import javax.inject.Named;
+
+import org.primefaces.event.RowEditEvent;
 
 import gruppe1.ejb.beans.SchoolBeanLocal;
 import gruppe1.ejbClient.entity.SchoolDTO;
@@ -41,6 +45,22 @@ public class SchoolOverview implements Serializable {
     	schoolDTO.setPhone(phone);
     	
     	school.create(schoolDTO);
+    }
+    
+    public void onRowEdit(RowEditEvent<SchoolDTO> event) {
+        FacesMessage msg = new FacesMessage("Car Edited " + event.getObject().getName());
+        FacesContext.getCurrentInstance().addMessage(null, msg);
+//
+//        SchoolDTO schoolDTO = new SchoolDTO();
+//        schoolDTO.setAddress(event.getObject().getAddress());
+//        schoolDTO.setCity(event.getObject().getCity());
+//        schoolDTO.setName(event.getObject().getName());
+//        schoolDTO.setPhone(event.getObject().getPhone());
+//        schoolDTO.setPostalNumber(event.getObject().getPostalNumber());
+//        schoolDTO.setSchoolId(event.getObject().getSchoolId());
+//        
+//        school.update(schoolDTO);
+//        
     }
 
     
