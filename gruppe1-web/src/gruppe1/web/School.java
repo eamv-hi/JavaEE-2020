06f008209dedf31341abff472e73ca14b3cb58f9
Kristian Klausen <kristian@klausen.dk>
2020-04-15 14:15:22 +0200

    gruppe1: Formater School.java

--- gruppe1-web/src/gruppe1/web/School.java
+++ gruppe1-web/src/gruppe1/web/School.java
@@ -7,30 +7,29 @@ import javax.inject.Named;
 import gruppe1.ejb.beans.SchoolBeanLocal;
 import gruppe1.ejbClient.entity.SchoolDTO;
 
-
 @Named("dtSchoolView")
 @RequestScoped
 public class School {
-		private String schoolId;
-		private SchoolDTO school;
-		
-	    public String getSchoolId() {
-	        return schoolId;
-	    }
-	 
-	    public void setSchoolId(String schoolId) {
-	        this.schoolId = schoolId;
-	    }
-		
-		@Inject
-		private SchoolBeanLocal schoolBean;
+	private String schoolId;
+	private SchoolDTO school;
 
-		public SchoolDTO getSchool() {
-			return schoolBean.get(Integer.parseInt(schoolId));
-		}
-		
-		public String delete() {
-			schoolBean.delete(Integer.parseInt(schoolId));
-			return "schoolOverview?faces-redirect=true";
-		}
+	public String getSchoolId() {
+		return schoolId;
 	}
+
+	public void setSchoolId(String schoolId) {
+		this.schoolId = schoolId;
+	}
+
+	@Inject
+	private SchoolBeanLocal schoolBean;
+
+	public SchoolDTO getSchool() {
+		return schoolBean.get(Integer.parseInt(schoolId));
+	}
+
+	public String delete() {
+		schoolBean.delete(Integer.parseInt(schoolId));
+		return "schoolOverview?faces-redirect=true";
+	}
+}
